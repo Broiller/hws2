b36f41cd5a3f39f7d76d444f80cbc4edd92c796d
Yauheni Haurylenka <djonkaupervud@mail.ru>
2023-02-02 13:24:11 +0300

    add hw4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,11 +18,12 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        + (disabled ? s.disabled : xType === 'red' ? s.red : s.default)
+        + (disabled ? s.disabled : xType === 'red' ? s.red : xType === 'secondary' ? s.secondary :s.default)
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
         <button
+
             disabled={disabled}
             className={finalClassName}
             {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
