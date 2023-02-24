ac4d68e30b5f10fcc8f5fc8c622fbaf7b9de4a5d
Yauheni Haurylenka <djonkaupervud@mail.ru>
2023-02-24 15:02:42 +0300

    add hw7

--- src/s2-homeworks/hw07/HW7.tsx
+++ src/s2-homeworks/hw07/HW7.tsx
@@ -18,7 +18,7 @@ const arr = [
 ] // value может быть изменено
 
 const HW7 = () => {
-    const [value, onChangeOption] = useState(1) // селект и радио должны работать синхронно
+    const [value, onChangeOption] = useState(arr[1].id) // селект и радио должны работать синхронно
 
     return (
         <div id={'hw7'}>
@@ -36,6 +36,7 @@ const HW7 = () => {
                         />
                     </div>
                     <div>
+
                         <SuperRadio
                             id={'hw7-super-radio'}
                             name={'hw7-radio'}
